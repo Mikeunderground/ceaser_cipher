f7da9b16d7df4b53ec4badaa3e840c55fcda0494
Michael <mikw1534@gmail.com>
2021-03-08 16:47:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// prompt for string to be cipher'd
+
+
+// construct array of aphabet 
+let alphabet = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+
+
+// break string down in to single chars
+
+
+// generate random number to .shift()
+
+
+// .find() each char and + along the array index with .shift()
+
+
+// recombine string
+
+
+// output string
