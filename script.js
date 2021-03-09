0e8fcaa32933fd9e15fe5c64d96576c08136362a
Mikeunderground <mikw1534@gmail.com>
2021-03-09 16:38:29 -0500

    iterated over both strings but am unable to ID duplicate charactrers

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
+// USE FUNCTION TO DO ALL THE HEAVY LIFTING
+let text = prompt("Input text to be ciphered");
+// take user inputted shift later
+let shift = 5;
+
+
+function ceasarCipher(text, shift){
+
+
+    
+}
+
 // generate random number to .shift()
-let rando = Math.floor(Math.random() * 10);
-console.log("random number is: " + rando);
+// let rando = Math.floor(Math.random() * 10);
+// console.log("random number is: " + rando);
 
 // prompt for string to be cipher'd
-let chars = " ";
-let message = prompt("Input a message you would like encoded.");
-message = message.toLowerCase();
-console.log(message);
-let encryptedMessage = " ";
-let initIndex = " ";
+// let chars = " ";
+// let message = prompt("Input a message you would like encoded.");
+// message = message.toLowerCase();
+// console.log(message);
+// let encryptedMessage = " ";
+// let initIndex = " ";
 
 // construct array of aphabet 
-let alphabet = "abcdefghijklmnopqrstuvwxyz";
+// let alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 // loop through message 
-for (let i = 0; i < message.length; i++){
-    // Goes through each letter of a string 
-    console.log(message[i]);
+// for (let i = 0; i < message.length; i++){
+//     // Goes through each letter of a string 
+//     console.log(message[i]);
     
-    // Loop through alpha to match chars and return indes
-    for (let i = 0; i < alphabet.length; i++){
+//     // Loop through alpha to match chars and return indes
+//     for (let i = 0; i < alphabet.length; i++){
         
         
-    };
+//     };
 
-    if( message[i] === alphabet[i]){
-        initIndex = alphabet.indexOf(alphabet[i]);
+//     if( message[i] === alphabet[i]){
+//         initIndex = alphabet.indexOf(alphabet[i]);
         
-    }; 
+//     }; 
   
-};
+// };
 
 
 
