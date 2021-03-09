1fd36b97997866b4e93d2552b9bad8b8f0456d03
Mikeunderground <mikw1534@gmail.com>
2021-03-09 02:57:32 -0500

    able to 1. loop over string 2. index each character to an alphabet array

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 // generate random number to .shift()
 let rando = Math.floor(Math.random() * 10);
+console.log("random number is: " + rando);
 
 // prompt for string to be cipher'd
-let chars = []
-let message = prompt("Input a message you would like encoded.").toLowerCase;
+let chars = " ";
+let message = prompt("Input a message you would like encoded.");
+message = message.toLowerCase();
 console.log(message);
 let encryptedMessage = " ";
 
 
 // construct array of aphabet 
-let alphabet = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 // loop through message to put all chars in to array
 for (var i = 0; i < message.length; i++){
-    console.log(i);
+    // Goes through each letter of a string 
+    console.log(message[i]);
+    
+    // find each letter in alphabet array
+    //console.log(alphabet.indexOf(message[i]));
+    chars = alphabet.indexOf(message[i]);
+    console.log(chars);
+    // seems to just take the index numbers and add them together
+
+    // for loop add rando to each numbers index
+
+    // create bring in encryptedMessage Variable and push new message there
 
     // break string down in to single chars and put in to an array
-    chars.push(i);
 
-    console.log(chars);
+    
+};
 
-    return chars;
-}
 
 // .indexOf() each char and + along the array index with .shift()
-for (var i = 0; i < chars.length; i++){
-    let position = alphabet.indexOf(i)
-    let newPosition = position + rando;
-    let pulledLettter = alphabet[newPosition];
-    encryptedMessage.append(pulledLetter);
-    return encryptedMessage;
-}
+// for (var i = 0; i < chars.length; i++){
+//     let position = alphabet.indexOf(i)
+//     let newPosition = position + rando;
+//     let pulledLettter = alphabet[newPosition];
+//     encryptedMessage.append(pulledLetter);
+// }
 
-console.log(rando);
 // recombine string
 
 
